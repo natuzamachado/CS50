1b28366a220551fc106a04d9fc8e227e773e4dae
Natuza Machado <63171826+natuzamachado@users.noreply.github.com>
2023-12-01 17:15:45 -0300

    fixed bug

--- prime.c
+++ prime.c
@@ -1,36 +1,63 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void) {
+bool isPrime(int num);
 
-    // get user input for minimum and a maximum number;
+int main(void)
+{
+    int minimum;
+    int maximum;
     
-    int minimum = get_int("Minimum: ");
+     // get user input for minimum and a maximum number;
+    // minimum and maximum should be positive numbers greater than zero;
 
-    int maximum = get_int("Maximum: ");
-
-    //minimum and maximum should be positive numbers greater than zero;
-    
     do
     {
         minimum = get_int("Minimum: ");
       
-    } while (minimum < 0);
+    } while (minimum <= 0);
 
     do
     {
         maximum = get_int("Maximum: ");
 
-    } while (maximum < 0);
-          
-    // prime is a number that is only divisible by 1 and itself
-    //print only prime numbers between minimum and maximum
-   
-        for (int i = minimum; i <= maximum; i++)
-        {   
-            if ( i != 1 && i % 2 != 0 || i == 2)
+    } while (maximum <= 0);
+    
+
+        // prime is a number that is only divisible by 1 and itself
+        // prints only prime numbers between minimum and maximum
+
+        for ( int i = minimum; i < maximum; i++ )
+        { 
+            if (isPrime(i))
             {
                 printf("%i\n", i);
             }
         }
 }
+
+
+//checks if a number is prime
+
+bool isPrime(int num)
+{
+    if (num == 1)
+    {
+        return false;
+    }
+
+    if (num == 2)
+    {
+        return true;
+    }
+
+    for (int i = 2; i < num; i++)
+    {
+        if ((num % i) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
